6f06decf568e3324750180eafde6e998b7dd8d88
T0rah <62261361+Torrahth@users.noreply.github.com>
2023-07-04 15:04:55 -0500

    forgot to add recipes to barebone

--- Items/Weapons/Melee/BareBones.cs
+++ Items/Weapons/Melee/BareBones.cs
@@ -19,12 +19,27 @@ namespace tm.Items.Weapons.Melee
             Item.knockBack = 2;
             Item.noUseGraphic = true;
             Item.UseSound = SoundID.Item1;
-            Item.damage = 12;
+            Item.damage = 38;
             Item.DamageType = DamageClass.Melee;
             Item.channel = true;
 
             Item.shoot = ModContent.ProjectileType<BareBonesFlail>();
             Item.shootSpeed = 7;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+    .AddIngredient(ItemID.FlamingMace)
+    .AddIngredient(ItemID.GoldBar, 6)
+         .AddIngredient(ItemID.Bone, 30)
+    .AddTile(TileID.Anvils)
+    .Register();
+            CreateRecipe()
+.AddIngredient(ItemID.FlamingMace)
+.AddIngredient(ItemID.PlatinumBar, 6)
+.AddIngredient(ItemID.Bone, 30)
+.AddTile(TileID.Anvils)
+.Register();
         }
     }
 }
